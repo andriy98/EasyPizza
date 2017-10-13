1b5302d62884b2769a20a9563d1e9b9c123e8a40
Andriy98 <rtdmytryshyn@gmail.com>
2017-10-14 02:07:33 +0300

    Add new variant of mylist_layout, add clean details_fragment

--- app/src/main/java/com/example/andrii/myapplication/CustomListAdapter.java
+++ app/src/main/java/com/example/andrii/myapplication/CustomListAdapter.java
@@ -1,9 +1,12 @@
 package com.example.andrii.myapplication;
 import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.squareup.picasso.Picasso;
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 
 public class CustomListAdapter extends ArrayAdapter {
     private Context context;
+    private Details_fragment details_fragment;
     private final ArrayList arrayList;
     private final ArrayList arrayList_photo;
     private final ArrayList arrayonline;
@@ -29,6 +33,15 @@ public class CustomListAdapter extends ArrayAdapter {
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView=inflater.inflate(R.layout.mylist, null,true);
+        details_fragment = new Details_fragment();
+        Button but_details = (Button) rowView.findViewById(R.id.button8);
+        but_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = ((ListTasks)context).getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container, details_fragment).commit();
+            }
+        });
         TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView);
         Picasso.with(context)
@@ -38,5 +51,6 @@ public class CustomListAdapter extends ArrayAdapter {
         return rowView;
 
     }
+
 
 }
